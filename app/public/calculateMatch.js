aef9aed5c721848e31fc2f6a681e410b3d20791f
graysonlee123 <graysonleegantek@gmail.com>
2019-08-17 17:32:16 -0400

    added full naviigation bar in top left

--- app/public/calculateMatch.js
+++ app/public/calculateMatch.js
@@ -6,15 +6,12 @@ $("#submit-button").on("click", function (e) {
 function calculateMatch() {
     const name = $("#name-input").val();
     const photo = $("#photo-url-input").val();
+    
     const scores = [];
-
-    console.log($(".dropdown"));
-
     $(".dropdown").each( function (index) {
-        scores.push($(this).val() );
+        const value = $(this).val();
+        scores.push($(this).val() || 0);
     });
-
-    scores.push();
 
     const userAnswers = {
         name: name,
@@ -23,6 +20,4 @@ function calculateMatch() {
     }
 
     $.post("api/friends", userAnswers);
-    
-    console.log(scores);
 }
