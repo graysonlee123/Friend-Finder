f7c17a052015bf915e168839bd4c0ae0495a1c13
graysonlee123 <graysonleegantek@gmail.com>
2019-08-17 15:50:12 -0400

    added main page, reset style, and main style

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,3 +6,6 @@ const path = require("path");
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/home.html")));
 
 app.get("/survey", (req, res) => res.sendFile(path.join(__dirname, "../public/survey.html")));
+
+app.get("/master.css", (req, res) => res.sendFile(path.join(__dirname, "../public/master.css")));
+app.get("/reset.css", (req, res) => res.sendFile(path.join(__dirname, "../public/reset.css")));
