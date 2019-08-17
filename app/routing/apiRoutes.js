020a981ce1268324cf6d619cbcb0ae22d1879cb4
graysonlee123 <graysonleegantek@gmail.com>
2019-08-17 12:29:52 -0400

    post requests to api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,4 +7,12 @@ const friends = require("../data/friends.js")
 
 app.get("/api/friends", (req, res) => res.json(friends) );
 
-app.post("/api/friends", (req, res) => console.log(req.body));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/api/friends", (req, res) => {
+    const newPost = req.body;
+    console.log(newPost);
+    res.json(newPost);
+    friends.push(newPost);
+});
