06dc3beefbc84c4a99493cfae5660e7d705e172c
Grayson Gantek <graysonleegantek@gmail.com>
2019-10-04 21:19:50 -0400

    starts moving survey funnctions into a survey variable as methods; succsesfully posting data to MongoDB

--- app/public/index.js
+++ app/public/index.js
@@ -9,17 +9,75 @@ const options = [
     'Strongly disagree'
 ];
 
-// Run on page load
-fetchAndDisplayQuestions();
-
 // Survey functions
-async function fetchAndDisplayQuestions() {
-    const questions = await $.getJSON('/data/questions.json').catch(() => {
-        console.log('There was an error loading in questions.json!');
-    });
+const survey = {
+    async fetchAndDisplayQuestions() {
+        const questions = await $.getJSON('/data/questions.json').catch(() => {
+            console.log('There was an error loading in questions.json!');
+        });
+    
+        if (questions) {
+            this.generateSurvey(questions);
+        }
+    },
 
-    if (questions) {
-        generateSurvey(questions);
+    generateSurvey(questions) {
+        const $textFields = $('#info-container');
+        const $questions = $('#questions-container');
+    
+        $textFields.append(
+            `<div class="text-input-box survey-row">
+                <label for="name-input" class="label required-field"><h2 id="required-name-input">Name</h2></label>
+                <input type="text" id="name-input" class="text-input" placeholder="Johnny Appleseed">
+            </div>`
+        );
+    
+        questions.forEach((question, i) => {
+            const $container = $(`<div class="question-box survey-row">`);
+            const $label = $(`<label for="question-${i}" class="label">`);
+            const $options = $(`<select name="question-${i}" value="0" id="question-${i}" class="dropdown">`);
+            
+            $label.append($(`<h2>Question ${i + 1}</h2>`), $(`<p>${question.question}</p>`));
+    
+            options.forEach((option, i) => {
+                if (option === "Neutral") $options.append($(`<option value="${i}" selected>${option}</option>`));
+                else $options.append($(`<option value="${i}">${option}</option>`));
+            });
+    
+            $container.append($label);
+            $container.append($options);
+    
+            $questions.append($container);
+        });
+    },
+
+    calculateMatch() {
+        console.log("hello");
+    },
+
+    badInputFeedback(elementId) {
+        console.log(elementId);
+    },
+
+    async gatherAndPushResults() {
+        const scores = [];
+
+        $('.dropdown').each(function() {
+            scores.push($(this).val() || '0');
+        });
+
+        const postData = await $.ajax({
+            url: '/api/friends',
+            method: 'POST',
+            data: {
+                name: 'todd',
+                scores: scores
+            }
+        }).catch((err) => console.log("Error posting scores!", err));
+
+        if (postData) {
+            console.log('pushed!');
+        }
     }
 }
 
@@ -58,59 +116,88 @@ function compare(userData) {
     });
 }
 
-function displayResults(chosenFriend, matchScore) {
+// function displayResults(chosenFriend, matchScore) {
 
-    matchScore = 100 - matchScore;
+//     matchScore = 100 - matchScore;
 
-    const page = $("#survey-body");
+//     const page = $("#survey-body");
 
-    const resutlsOverlay = $(`<div id="results-overlay">`);
+//     const resutlsOverlay = $(`<div id="results-overlay">`);
 
-    const resultsContainer = $(`<div id="results-container" class="gradient-bg">
-        <div>
-            <h1 class="results-h1">found a friend!</h1>
-            <p class="results-p">You would be good friends with:</p>
-        </div>
-        <div id="results-display">
-            <h1 class="results-h1">${chosenFriend.name}</h1>
-            <div class="dispaly-img-container">
-                <img src="${chosenFriend.photo}" alt="${chosenFriend.name}" class="results-img" onerror="this.onerror=null;this.src='http://via.placeholder.com/200'">
-            </div>
-            <p>Your match score is ${matchScore} points!</p>
-            <a href="/" class="button">Home</a>
-        </div>
-    </div>`);
-    resutlsOverlay.append(resultsContainer);
+//     const resultsContainer = $(`<div id="results-container" class="gradient-bg">
+//         <div>
+//             <h1 class="results-h1">found a friend!</h1>
+//             <p class="results-p">You would be good friends with:</p>
+//         </div>
+//         <div id="results-display">
+//             <h1 class="results-h1">${chosenFriend.name}</h1>
+//             <div class="dispaly-img-container">
+//                 <img src="${chosenFriend.photo}" alt="${chosenFriend.name}" class="results-img" onerror="this.onerror=null;this.src='http://via.placeholder.com/200'">
+//             </div>
+//             <p>Your match score is ${matchScore} points!</p>
+//             <a href="/" class="button">Home</a>
+//         </div>
+//     </div>`);
+//     resutlsOverlay.append(resultsContainer);
 
-    page.append(resutlsOverlay);
-}
+//     page.append(resutlsOverlay);
+// }
 
-function generateSurvey(questions) {
-    const $textFields = $('#info-container');
-    const $questions = $('#questions-container');
+const nameInputContainer = $("#required-name-input");
+const photoInputContainer = $("#required-photo-url-input");
 
-    $textFields.append(
-        `<div class="text-input-box survey-row">
-            <label for="name-input" class="label required-field"><h2 id="required-name-input">Name</h2></label>
-            <input type="text" id="name-input" class="text-input" placeholder="Johnny Appleseed">
-        </div>`
-    );
+let nameValue = null;
+let photoValue = null;
 
-    questions.forEach((question, i) => {
-        const $container = $(`<div class="question-box survey-row">`);
-        const $label = $(`<label for="question-${i}" class="label">`);
-        const $options = $(`<select name="question-${i}" id="question-${i}" class="dropdown">`);
-        
-        $label.append($(`<h2>Question ${i + 1}</h2>`), $(`<p>${question.question}</p>`));
+let validImage = false;
+let validName = false;
 
-        options.forEach((option, i) => {
-            if (option === "Neutral") $options.append($(`<option value="${i}" selected>${option}</option>`));
-            else $options.append($(`<option value="${i}">${option}</option>`));
-        });
+function buildUserObj() {
+    const scores = [];
 
-        $container.append($label);
-        $container.append($options);
-
-        $questions.append($container);
+    $(".dropdown").each(function (index) {
+        const value = $(this).val();
+        scores.push($(this).val() || 0);
     });
+
+    return {
+        name: nameValue,
+        photo: photoValue,
+        scores: scores
+    }
 }
+
+function highlightNameField() {
+    nameInputContainer.addClass("red");
+}
+
+function refreshFieldHighlights() {
+    nameInputContainer.removeClass("red");
+    photoInputContainer.removeClass("red");
+}
+
+function scroll() {
+    $("html, body").animate({ scrollTop: $('#survey-div').offset().top }, 1000);
+}
+
+function checkInputs() {
+    nameValue = $("#name-input").val();
+
+    if (!nameValue) {
+        highlightNameField();
+        scroll();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Run on page load
+survey.fetchAndDisplayQuestions();
+
+// Event listeners
+$(document).on('click', '#submit-button', function (e) {
+    e.preventDefault();
+    survey.gatherAndPushResults();
+    // if (checkInputs()) calculateMatch(buildUserObj());
+});
