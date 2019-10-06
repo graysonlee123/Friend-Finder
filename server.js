a946bdf6644a376d899070df46e207eee2e07824
graysonlee123 <graysonleegantek@gmail.com>
2019-10-06 12:15:12 -0400

    sets up for heroku deployment

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8000;
+const db = mongoose.connection;
+const {NODE_ENV, TEST_DB, DEVELOPMENT_DB, MONGODB_URI} = process.env;
+
+let dbString;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './app/public')));
 app.use('/uploads', express.static('uploads'));
 
-mongoose.connect('mongodb://localhost/friend-match', {useNewUrlParser: true});
-const db = mongoose.connection;
+if (NODE_ENV === 'test') dbString = TEST_DB;
+else if (NODE_ENV === 'development') dbString = DEVELOPMENT_DB;
+else if (NODE_ENV === 'production') dbString = MONGODB_URI;
+else throw console.log("Needs a database to connect to!");
+
+mongoose.connect(dbString, {useNewUrlParser: true});
 
 require('./app/routing/api.friends')(app);
 require('./app/routing/api.images')(app);
 require('./app/routing/routes.html')(app);
-
-const PORT = process.env.PORT || 8000;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.on('open', () => {
